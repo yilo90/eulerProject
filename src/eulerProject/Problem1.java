f6eb33b5144c9a41ade99e734a87169d6ec674c9
YZAN <YZAN@20H0376.fritz.box>
2020-11-05 17:13:47 +0100

    Problem1

--- src/eulerProject/Problem1.java
+++ src/eulerProject/Problem1.java
@@ -1,9 +1,21 @@
 package eulerProject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Integer> list=new ArrayList<Integer>();
+		for (int i=1; i<1000;i++) {
+			if (i%3==0 || i%5==0) {
+				list.add(i);
+			}
+		}
+
+		int sum=list.stream().mapToInt(i -> i).sum();
+		System.out.println(sum);
+		
 
 	}
 
